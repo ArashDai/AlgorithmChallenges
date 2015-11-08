607ec12bf6200013a65191ce2916ff5e977f66de
Arash Dai <arashdai@gmail.com>
2015-11-08 01:54:31 -0800

    doubly linked list

---
+++ DataStructures/doubly-linked-list.js
@@ -0,0 +1,57 @@
+//Create a doubly linked list with methods
+ 
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+}
+
+
+ 
+LinkedList.prototype.add = function(val) {
+  var node = new Node(val);
+  if(this.head === null){
+    this.head = node;
+    this.tail = node;
+  }
+  else{
+    var current = this.head  
+    while(current.next){
+      current = current.next
+    }
+    current.next = node;
+  }
+}  
+  
+
+LinkedList.prototype.remove = function(val) {
+  // body...
+}
+
+
+LinkedList.prototype.contains = function(val) {
+  var position = 0;
+  var current = this.head;
+
+  if (current.val === val ){return true}
+    
+  while(current.next){
+    if (current.val === val ){return true}
+    else{
+      current = current.next
+    }
+
+  }
+return false
+}
+
+
+
+
+
+
